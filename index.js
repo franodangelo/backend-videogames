bb718ef43fbf9da3c3ea4359a0cab18b7fc7ebc4
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-30 12:56:06 -0300

    config port for railway

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const { DB_PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(`${ DB_PORT }`, () => {
+  server.listen(`0.0.0.0:${ DB_PORT }`, () => {
     console.log(`%s listening at port ${DB_PORT}`);
   })
 })
