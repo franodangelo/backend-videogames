2e4ad01b716826b1d2eb713c6dd4e7999c7afffc
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-13 12:00:36 -0300

    updated following all learning paths

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.DB_PORT || 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log(`%s listening at ${port}`); 
   })
 })
